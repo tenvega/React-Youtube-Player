fc7770c787d374fc44304e5dfd5ad3493f16db08
Enoc <etorra@gmail.com>
2017-09-20 18:36:17 -0400

    created components

---
+++ src/components/video_list_item.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const VideoListItem = ({video}) => {
+const imageURl = video.snippet.thumbnails.default.url;
+
+  return (
+
+          <li className = "list-group-item">
+            <div className = "video-list media">
+              <div className = "video-list media-left">
+               <img className = "media-object" src ={imageURl}/>
+            </div>
+              <div className = "media-body">
+               <div className = 'media-heading'>{video.snippet.title}</div>
+            </div>
+            </div>
+          </li>
+
+  );
+};
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+const API_KEY ='AIzaSyDixVl-5F_3glbasaMVTjCYWhZtPkOIHiA';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+  constructor(props){
+    super(props);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    this.state ={ videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) =>{
+    this.setState({ videos });
+    // console.log(videos);
+    // this.setState({videos: videos});
+    });
+  }
+  render() {
+  return(
+       <div>
+         <SearchBar/>
+         <VideoList videos ={this.state.videos}/>
+       </div>
+ );
+}
+}
+
+ReactDom.render(<App/>,document.querySelector('.container'));
