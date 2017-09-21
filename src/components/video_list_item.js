95612b92f2141b7ee38f4392e0e8cdc70258c377
Enoc <etorra@gmail.com>
2017-09-20 22:51:30 -0400

    app is functional/added styling

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
 const imageURl = video.snippet.thumbnails.default.url;
 
   return (
 
-          <li className = "list-group-item">
+          <li onClick ={() => onVideoSelect(video)} className = "list-group-item">
             <div className = "video-list media">
               <div className = "video-list media-left">
                <img className = "media-object" src ={imageURl}/>
